29a1a455b31d0388d57a0918381ce0164b4a76e9
Abhilash <abhilashmeher1234@gmail.com>
2022-04-05 09:49:02 +0530

    Added program file for Prime number

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prime Number from 0-1000 are: ");
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                int div = 1;
+                int count = 0;
+                while (div<=i)
+                {
+                    if (i % div == 0)
+                    {
+                        count++;
+                    }
+                    div++;
+                }
+
+                if (count == 2)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
