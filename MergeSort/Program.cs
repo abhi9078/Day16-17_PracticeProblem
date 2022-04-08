8b43f3452029f18781930206b98c79449af52060
Abhilash <abhilashmeher1234@gmail.com>
2022-04-08 18:55:27 +0530

    Added program file for MergeSort

---
+++ MergeSort/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace MergeSort
+{
+    internal class Program
+    {
+        public void merge(int[] arr, int l, int m, int r)
+        {
+            // Find sizes of two
+            // subarrays to be merged
+            int n1 = m - l + 1;
+            int n2 = r - m;
+
+            // Create temp arrays
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+            int i, j;
+
+            // Copy data to temp arrays
+            for (i = 0; i < n1; ++i)
+                L[i] = arr[l + i];
+            for (j = 0; j < n2; ++j)
+                R[j] = arr[m + 1 + j];
+
+
+            i = 0;
+            j = 0;
+            int k = l;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+
+            // Copy remaining elements
+            // of L[] if any
+            while (i < n1)
+            {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+
+            // Copy remaining elements
+            // of R[] if any
+            while (j < n2)
+            {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+
+        //Sorting method
+        public void Sort(int[] arr, int l, int r)
+        {
+            if (l < r)
+            {
+                // Find the middle
+                // point
+                int m = l + (r - l) / 2;
+
+                // Sort first and
+                // second halves
+                Sort(arr, l, m);
+                Sort(arr, m + 1, r);
+
+                // Merge the sorted halves
+                merge(arr, l, m, r);
+            }
+        }
+
+       //Display Method
+        public static void PrintArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine();
+        }
+
+        //Driver Method
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Console.WriteLine("Welcome to MergeSort program");
+            Console.WriteLine("Enter the Length of Array");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+            Console.WriteLine("Enter Array Elements");
+            for(int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            program.Sort(arr, 0, n-1);
+            Console.WriteLine("Sorted Array is: ");
+            Program.PrintArray(arr);
+
+        }
+    }
+}
