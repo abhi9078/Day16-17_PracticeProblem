d8e04ac244674af11f26f38d25396d10ffda82b3
Abhilash <abhilashmeher1234@gmail.com>
2022-04-08 18:35:30 +0530

    Added program file for Bubble sort

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    internal class Program
+    {
+        static void BubbleSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+
+                    if (arr[j] > arr[j + 1])
+                    {
+                        // swap temp and arr[i]
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+
+            //Display unit
+            Console.WriteLine("Sorted array is: ");
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        //Driver method
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to BubbleSort Program");
+            Console.WriteLine("Please enter the length of the Array");
+            int n= int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            Console.WriteLine("Please enter the array elements");
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Program.BubbleSort(arr);
+            
+        }
+    }
+}
